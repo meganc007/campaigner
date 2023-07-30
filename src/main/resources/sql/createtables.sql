c66fe63a9e41e0f96d47a34e36a5c7b4d17f40c1
Megan Commings <meganc007@msn.com>
2023-07-29 19:28:20 -0500

    worked on more table relationships

--- src/main/resources/sql/createtables.sql
+++ src/main/resources/sql/createtables.sql
@@ -1,3 +1,9 @@
+drop table if exists months;
+drop table if exists days;
+drop table if exists moons;
+drop table if exists suns;
+drop table if exists celestial_events;
+drop table if exists calendar;
 drop table if exists terrains;
 drop table if exists settlement_types;
 drop table if exists governments;
@@ -22,6 +28,61 @@ drop table if exists weapons;
 drop table if exists weapon_damages;
 drop table if exists inventory;
 drop table if exists events_places_people;
+drop table if exists monsters;
+drop table if exists hooks;
+drop table if exists hooks_places_people;
+drop table if exists objectives;
+drop table if exists objectives_places_people_items_weapons;
+drop table if exists outcomes;
+drop table if exists outcomes_places_people_items_weapons;
+drop table if exists rewards;
+drop table if exists conditionals;
+drop table if exists quests;
+
+create table months (
+	id int generated always as identity,
+	name varchar not null,
+	primary key(id)
+);
+
+create table days (
+	id int generated always as identity,
+	name varchar not null,
+	primary key(id)
+);
+
+create table moons (
+	id int generated always as identity,
+	name varchar not null,
+	primary key(id)
+);
+
+create table suns (
+	id int generated always as identity,
+	name varchar not null,
+	primary key(id)
+);
+
+create table celestial_events (
+	id int generated always as identity,
+	name varchar not null,
+	constraint fk_moon foreign key(id) references moons(id),
+	constraint fk_sun foreign key(id) references suns(id),
+	primary key(id)
+);
+
+create table calendar (
+	id int generated always as identity,
+	year int not null,
+	daysInYear int,
+	daysInMonth int,
+	daysInWeek int,
+	constraint fk_month foreign key(id) references months(id),
+	weekNum int,
+	constraint fk_day foreign key(id) references days(id),
+	constraint fk_celestial_events foreign key(id) references celestial_events(id),
+	primary key(id)
+);
 
 create table terrains (
 	id int generated always as identity, 
@@ -166,7 +227,9 @@ create table items (
 	id int generated always as identity,
 	name varchar not null,
 	description varchar,
-	value numeric,
+	goldValue numeric,
+	silverValue numeric,
+	copperValue numeric,
 	weight numeric,
 	constraint fk_type foreign key(id) references item_types(id),
 	isMagical bool,
@@ -193,7 +256,9 @@ create table weapons (
 	id int generated always as identity,
 	name varchar not null,
 	description varchar,
-	value numeric,
+	goldValue numeric,
+	silverValue numeric,
+	copperValue numeric,
 	weight numeric,
 	constraint fk_type foreign key(id) references weapon_types(id),
 	isMagical bool,
@@ -221,9 +286,100 @@ create table inventory (
 create table events_places_people (
 	id int generated always as identity,
 	constraint fk_person foreign key(id) references people(id),
+	constraint fk_event foreign key(id) references events(id),
 	constraint fk_places foreign key(id) references places(id),
-	constraint fk_city foreign key(id) references cities(id),
-	constraint fk_continent foreign key(id) references continents(id),
-	constraint fk_country foreign key(id) references countries(id),
+	primary key(id)
+);
+
+create table monsters (
+	id int generated always as identity,
+	name varchar not null,
+	title varchar,
+	isEnemy bool not null,
+	personality varchar,
+	description varchar,
+	notes varchar,
+	primary key(id)
+);
+
+create table hooks (
+	id int generated always as identity,
+	description varchar not null,
+	notes varchar,
+	primary key(id)
+);
+
+create table hooks_places_people (
+	id int generated always as identity,
+	constraint fk_hook foreign key(id) references hooks(id),
+	constraint fk_person foreign key(id) references people(id),
+	constraint fk_places foreign key(id) references places(id),
+	primary key(id)
+);
+
+create table objectives (
+	id int generated always as identity,
+	description varchar not null,
+	notes varchar,
+	primary key(id)
+);
+
+create table objectives_places_people_items_weapons (
+	id int generated always as identity,
+	constraint fk_objective foreign key(id) references objectives(id),
+	constraint fk_item foreign key(id) references items(id),
+	constraint fk_weapon foreign key(id) references weapons(id),
+	constraint fk_person foreign key(id) references people(id),
+	constraint fk_places foreign key(id) references places(id),
+	primary key(id)
+);
+
+create table outcomes (
+	id int generated always as identity,
+	description varchar not null,
+	notes varchar,
+	primary key(id)
+);
+
+create table outcomes_places_people_items_weapons (
+	id int generated always as identity,
+	constraint fk_outcome foreign key(id) references outcomes(id),
+	constraint fk_item foreign key(id) references items(id),
+	constraint fk_weapon foreign key(id) references weapons(id),
+	constraint fk_person foreign key(id) references people(id),
+	constraint fk_places foreign key(id) references places(id),
+	primary key(id)
+);
+
+create table rewards (
+	id int generated always as identity,
+	description varchar not null,
+	notes varchar,
+	goldValue numeric,
+	silverValue numeric,
+	copperValue numeric,
+	constraint fk_item foreign key(id) references items(id),
+	constraint fk_weapon foreign key(id) references weapons(id),
+	primary key(id)
+);
+
+create table conditionals (
+	id int generated always as identity,
+	description varchar not null,
+	notes varchar,
+	constraint fk_objectives_places_people_items_weapons foreign key(id) references objectives_places_people_items_weapons(id),
+	wasObjectiveCompleted bool,
+	constraint fk_outcomes_places_people_items_weapons foreign key(id) references outcomes_places_people_items_weapons(id),
+	constraint fk_reward foreign key(id) references rewards(id),
+	primary key(id)
+);
+
+create table quests (
+	id int generated always as identity,
+	description varchar not null,
+	notes varchar,
+	constraint fk_hooks_places_people foreign key(id) references hooks_places_people(id),
+	constraint fk_conditional foreign key(id) references conditionals(id),
+	constraint fk_reward foreign key(id) references rewards(id),
 	primary key(id)
 );
