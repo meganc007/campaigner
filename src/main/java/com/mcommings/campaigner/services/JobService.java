fb01c54d97f0693dbb359372d095e5dd244ebf41
Megan Commings <meganc007@msn.com>
2023-08-16 17:09:58 -0500

    jobs: added the save, delete, and update functionality. all unit tests passing now

--- src/main/java/com/mcommings/campaigner/services/JobService.java
+++ src/main/java/com/mcommings/campaigner/services/JobService.java
@@ -2,8 +2,11 @@ package com.mcommings.campaigner.services;
 
 import com.mcommings.campaigner.interfaces.IJob;
 import com.mcommings.campaigner.models.Job;
+import com.mcommings.campaigner.models.RepositoryHelper;
 import com.mcommings.campaigner.models.repositories.IJobRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,17 +25,36 @@ public class JobService implements IJob {
     }
 
     @Override
-    public void saveJob(Job job) {
-
+    @Transactional
+    public void saveJob(Job job) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.nameIsNullOrEmpty(job)) {
+            throw new IllegalArgumentException("Job name cannot be null or empty.");
+        }
+        if(RepositoryHelper.nameAlreadyExists(jobRepository, job)) {
+            throw new DataIntegrityViolationException("Job already exists.");
+        }
+        jobRepository.saveAndFlush(job);
     }
 
     @Override
-    public void deleteJob(int jobId) {
+    @Transactional
+    public void deleteJob(int jobId) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.cannotFindId(jobRepository, jobId)) {
+            throw new IllegalArgumentException("Unable to delete; This job was not found.");
+        }
+        //TODO: check if foreign key
 
+        jobRepository.deleteById(jobId);
     }
 
     @Override
+    @Transactional
     public void updateJob(int jobId, Job job) {
-
+        if(RepositoryHelper.cannotFindId(jobRepository, jobId)) {
+            throw new IllegalArgumentException("Unable to update; This job was not found.");
+        }
+        Job jobToUpdate = RepositoryHelper.getById(jobRepository, jobId);
+        jobToUpdate.setName(job.getName());
+        jobToUpdate.setDescription(job.getDescription());
     }
 }
