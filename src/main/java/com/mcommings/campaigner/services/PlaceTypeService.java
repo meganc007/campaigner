6b39f91b92a802cc58e67054436d1cdf96d1fe43
Megan Commings <meganc007@msn.com>
2023-08-16 17:36:34 -0500

    placetypes: added the save, delete, and update functionality. all unit tests passing now

--- src/main/java/com/mcommings/campaigner/services/PlaceTypeService.java
+++ src/main/java/com/mcommings/campaigner/services/PlaceTypeService.java
@@ -2,9 +2,11 @@ package com.mcommings.campaigner.services;
 
 import com.mcommings.campaigner.interfaces.IPlaceType;
 import com.mcommings.campaigner.models.PlaceType;
+import com.mcommings.campaigner.models.RepositoryHelper;
 import com.mcommings.campaigner.models.repositories.IPlaceTypesRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,19 +26,35 @@ public class PlaceTypeService implements IPlaceType {
 
     @Override
     @Transactional
-    public void savePlaceType(PlaceType placeType) {
-
+    public void savePlaceType(PlaceType placeType) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.nameIsNullOrEmpty(placeType)) {
+            throw new IllegalArgumentException("Place Type name cannot be null or empty.");
+        }
+        if(RepositoryHelper.nameAlreadyExists(placeTypesRepository, placeType)) {
+            throw new DataIntegrityViolationException("Place Type already exists.");
+        }
+        placeTypesRepository.saveAndFlush(placeType);
     }
 
     @Override
     @Transactional
-    public void deletePlaceType(int placeTypeId) {
+    public void deletePlaceType(int placeTypeId) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.cannotFindId(placeTypesRepository, placeTypeId)) {
+            throw new IllegalArgumentException("Unable to delete; This place type was not found.");
+        }
+        //TODO: check if foreign key
 
+        placeTypesRepository.deleteById(placeTypeId);
     }
 
     @Override
     @Transactional
-    public void updatePlaceType(int placeTypeId, PlaceType placeType) {
-
+    public void updatePlaceType(int placeTypeId, PlaceType placeType) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.cannotFindId(placeTypesRepository, placeTypeId)) {
+            throw new IllegalArgumentException("Unable to update; This place type was not found.");
+        }
+        PlaceType placeTypeToUpdate = RepositoryHelper.getById(placeTypesRepository, placeTypeId);
+        placeTypeToUpdate.setName(placeType.getName());
+        placeTypeToUpdate.setDescription(placeType.getDescription());
     }
 }
