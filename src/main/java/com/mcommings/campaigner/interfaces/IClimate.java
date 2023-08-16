26c6fcb82f5b25c9f3ec2b247b0e9b399fae6ffb
Megan Commings <meganc007@msn.com>
2023-08-16 15:05:04 -0500

    fleshing out Climate endpoints
    Added the rest of the CRUD operations to the Climate interface and controller. Added the implementations to the Climate service and worked on saveClimate(). Added the unit tests. I'd like to try to lean more towards TDD as I continue working on this project.

--- src/main/java/com/mcommings/campaigner/interfaces/IClimate.java
+++ src/main/java/com/mcommings/campaigner/interfaces/IClimate.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface IClimate {
 
     public List<Climate> getClimates();
+
+    public void saveClimate(Climate climate);
+
+    public void deleteClimate(int id);
+
+    public void updateClimate(int id, Climate climate);
 }
--- src/main/java/com/mcommings/campaigner/services/ClimateService.java
+++ src/main/java/com/mcommings/campaigner/services/ClimateService.java
@@ -2,8 +2,11 @@ package com.mcommings.campaigner.services;
 
 import com.mcommings.campaigner.interfaces.IClimate;
 import com.mcommings.campaigner.models.Climate;
+import com.mcommings.campaigner.models.RepositoryHelper;
 import com.mcommings.campaigner.models.repositories.IClimateRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +21,28 @@ public class ClimateService implements IClimate {
     @Override
     public List<Climate> getClimates() {
         return climateRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void saveClimate(Climate climate) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.nameIsNullOrEmpty(climate)) {
+            throw new IllegalArgumentException("Climate name cannot be null or empty.");
+        }
+        if(RepositoryHelper.nameAlreadyExists(climateRepository, climate)) {
+            throw new DataIntegrityViolationException("Climate already exists.");
+        }
+
+        climateRepository.saveAndFlush(climate);
+    }
+
+    @Override
+    public void deleteClimate(int id) {
+
+    }
+
+    @Override
+    public void updateClimate(int id, Climate climate) {
+
     }
 }
