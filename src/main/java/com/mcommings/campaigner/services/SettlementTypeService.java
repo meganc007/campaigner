001d3632bb6d6936d22ec0da0272b76195163065
Megan Commings <meganc007@msn.com>
2023-08-17 18:39:30 -0500

    settlementtypes: added the save, delete, and update functionality. all unit tests passing now

--- src/main/java/com/mcommings/campaigner/services/SettlementTypeService.java
+++ src/main/java/com/mcommings/campaigner/services/SettlementTypeService.java
@@ -1,10 +1,12 @@
 package com.mcommings.campaigner.services;
 
 import com.mcommings.campaigner.interfaces.ISettlementType;
+import com.mcommings.campaigner.models.RepositoryHelper;
 import com.mcommings.campaigner.models.SettlementType;
 import com.mcommings.campaigner.models.repositories.ISettlementTypeRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,19 +26,36 @@ public class SettlementTypeService implements ISettlementType {
 
     @Override
     @Transactional
-    public void saveSettlementType(SettlementType settlementType) {
+    public void saveSettlementType(SettlementType settlementType) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.nameIsNullOrEmpty(settlementType)) {
+            throw new IllegalArgumentException("Settlement Type name cannot be null or empty.");
+        }
+        if(RepositoryHelper.nameAlreadyExists(settlementTypeRepository, settlementType)) {
+            throw new DataIntegrityViolationException("Settlement Type already exists.");
+        }
 
+        settlementTypeRepository.saveAndFlush(settlementType);
     }
 
     @Override
     @Transactional
-    public void deleteSettlementType(int settlementTypeId) {
+    public void deleteSettlementType(int settlementTypeId) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.cannotFindId(settlementTypeRepository, settlementTypeId)) {
+            throw new IllegalArgumentException("Unable to delete; This settlement type was not found.");
+        }
+        //TODO: check if foreign key
 
+        settlementTypeRepository.deleteById(settlementTypeId);
     }
 
     @Override
     @Transactional
     public void updateSettlementType(int settlementTypeId, SettlementType settlementType) {
-
+        if(RepositoryHelper.cannotFindId(settlementTypeRepository, settlementTypeId)) {
+            throw new IllegalArgumentException("Unable to update; This settlement type was not found.");
+        }
+        SettlementType settlementTypeToUpdate = RepositoryHelper.getById(settlementTypeRepository, settlementTypeId);
+        settlementTypeToUpdate.setName(settlementType.getName());
+        settlementTypeToUpdate.setDescription(settlementType.getDescription());
     }
 }
