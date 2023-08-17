41d94a3d9244de0b531682e16ddbb8e43759b008
Megan Commings <meganc007@msn.com>
2023-08-17 18:55:07 -0500

    terrain: added the save, delete, and update functionality. all unit tests passing now

--- src/main/java/com/mcommings/campaigner/services/TerrainService.java
+++ src/main/java/com/mcommings/campaigner/services/TerrainService.java
@@ -1,6 +1,7 @@
 package com.mcommings.campaigner.services;
 
 import com.mcommings.campaigner.interfaces.ITerrain;
+import com.mcommings.campaigner.models.RepositoryHelper;
 import com.mcommings.campaigner.models.Terrain;
 import com.mcommings.campaigner.models.repositories.ITerrainRepository;
 import jakarta.transaction.Transactional;
@@ -25,18 +26,36 @@ public class TerrainService implements ITerrain {
     @Override
     @Transactional
     public void saveTerrain(Terrain terrain) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.nameIsNullOrEmpty(terrain)) {
+            throw new IllegalArgumentException("Terrain name cannot be null or empty.");
+        }
+        if(RepositoryHelper.nameAlreadyExists(terrainRepository, terrain)) {
+            throw new DataIntegrityViolationException("Terrain already exists.");
+        }
 
+        terrainRepository.saveAndFlush(terrain);
     }
 
     @Override
     @Transactional
     public void deleteTerrain(int terrainId) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.cannotFindId(terrainRepository, terrainId)) {
+            throw new IllegalArgumentException("Unable to delete; This terrain not found.");
+        }
+        //TODO: check if foreign key
 
+        terrainRepository.deleteById(terrainId);
     }
 
     @Override
     @Transactional
     public void updateTerrain(int terrainId, Terrain terrain) throws IllegalArgumentException, DataIntegrityViolationException {
+        if(RepositoryHelper.cannotFindId(terrainRepository, terrainId)) {
+            throw new IllegalArgumentException("Unable to update; This terrain not found.");
+        }
 
+        Terrain terrainToUpdate = RepositoryHelper.getById(terrainRepository, terrainId);
+        terrainToUpdate.setName(terrain.getName());
+        terrainToUpdate.setDescription(terrain.getDescription());
     }
 }
