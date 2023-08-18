93303da3a11aeb0750b2594c91a3d58e5b6d4a75
Megan Commings <meganc007@msn.com>
2023-08-17 19:09:21 -0500

    wealth: added unit tests, CRUD endpoints

--- src/main/java/com/mcommings/campaigner/controllers/WealthController.java
+++ src/main/java/com/mcommings/campaigner/controllers/WealthController.java
@@ -3,9 +3,7 @@ package com.mcommings.campaigner.controllers;
 import com.mcommings.campaigner.models.Wealth;
 import com.mcommings.campaigner.services.WealthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,20 @@ public class WealthController {
     @GetMapping
     List<Wealth> getWealth() {
         return wealthService.getWealth();
+    }
+
+    @PostMapping
+    void saveWealth(@RequestBody Wealth wealth) {
+        wealthService.saveWealth(wealth);
+    }
+
+    @DeleteMapping(path = "{wealthId}")
+    void deleteWealth(@PathVariable int wealthId) {
+        wealthService.deleteWealth(wealthId);
+    }
+
+    @PutMapping(path = "{wealthId}")
+    void updateWealth(@PathVariable int wealthId, @RequestBody Wealth wealth) {
+        wealthService.updateWealth(wealthId, wealth);
     }
 }
--- src/main/java/com/mcommings/campaigner/services/WealthService.java
+++ src/main/java/com/mcommings/campaigner/services/WealthService.java
@@ -1,9 +1,11 @@
 package com.mcommings.campaigner.services;
 
 import com.mcommings.campaigner.interfaces.IWealth;
-import com.mcommings.campaigner.models.repositories.IWealthRepository;
 import com.mcommings.campaigner.models.Wealth;
+import com.mcommings.campaigner.models.repositories.IWealthRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +20,23 @@ public class WealthService implements IWealth {
     @Override
     public List<Wealth> getWealth() {
         return wealthRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void saveWealth(Wealth wealth) throws IllegalArgumentException, DataIntegrityViolationException {
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteWealth(int wealthId) throws IllegalArgumentException, DataIntegrityViolationException {
+
+    }
+
+    @Override
+    @Transactional
+    public void updateWealth(int wealthId, Wealth wealth) throws IllegalArgumentException, DataIntegrityViolationException {
+
     }
 }
