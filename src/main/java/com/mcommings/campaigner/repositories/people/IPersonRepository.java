df4c2ee83e6be29b64be978fd93aa8ec8451e3f4
Megan Commings <meganc007@msn.com>
2023-09-02 14:13:26 -0500

    Person: filled out the rest of the CRUD operations & tests. need to finish fk checks when tables that use Person as fk are added

--- src/main/java/com/mcommings/campaigner/repositories/people/IPersonRepository.java
+++ src/main/java/com/mcommings/campaigner/repositories/people/IPersonRepository.java
@@ -3,6 +3,7 @@ package com.mcommings.campaigner.repositories.people;
 import com.mcommings.campaigner.models.people.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,12 @@ public interface IPersonRepository extends JpaRepository<Person, Integer> {
     Optional<Person> findByFirstName(String firstName);
 
     Optional<Person> findByLastName(String lastName);
+
+    @Query("SELECT p FROM Person p WHERE " +
+            "p.firstName = :#{#person.firstName} AND " +
+            "p.lastName = :#{#person.lastName} AND " +
+            "p.fk_race = :#{#person.fk_race}")
+    Optional<Person> personExists(@Param("person") Person person);
 
     @Query("SELECT p FROM Person p WHERE p.fk_race = :id")
     List<Person> findByfk_race(int id);
--- src/main/java/com/mcommings/campaigner/services/people/PersonService.java
+++ src/main/java/com/mcommings/campaigner/services/people/PersonService.java
@@ -1,18 +1,24 @@
 package com.mcommings.campaigner.services.people;
 
 import com.mcommings.campaigner.interfaces.people.IPerson;
+import com.mcommings.campaigner.models.RepositoryHelper;
 import com.mcommings.campaigner.models.people.Person;
 import com.mcommings.campaigner.repositories.IWealthRepository;
 import com.mcommings.campaigner.repositories.people.IAbilityScoreRepository;
 import com.mcommings.campaigner.repositories.people.IPersonRepository;
 import com.mcommings.campaigner.repositories.people.IRaceRepository;
-import jakarta.persistence.Table;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.mcommings.campaigner.enums.ErrorMessage.*;
+import static java.util.Objects.isNull;
 
 @Service
 public class PersonService implements IPerson {
@@ -39,18 +45,70 @@ public class PersonService implements IPerson {
     @Override
     @Transactional
     public void savePerson(Person person) throws IllegalArgumentException, DataIntegrityViolationException {
+        if (nameIsNullOrEmpty(person.getFirstName()) || nameIsNullOrEmpty(person.getLastName())) {
+            throw new IllegalArgumentException(NULL_OR_EMPTY.message);
+        }
+        if (personAlreadyExists(person)) {
+            throw new DataIntegrityViolationException(NAME_EXISTS.message);
+        }
+        if (hasForeignKeys(person) &&
+                RepositoryHelper.foreignKeyIsNotValid(personRepository, getListOfForeignKeyRepositories(), person)) {
+            throw new DataIntegrityViolationException(INSERT_FOREIGN_KEY.message);
+        }
 
+        personRepository.saveAndFlush(person);
     }
 
     @Override
     @Transactional
     public void deletePerson(int personId) throws IllegalArgumentException, DataIntegrityViolationException {
-
+        if (RepositoryHelper.cannotFindId(personRepository, personId)) {
+            throw new IllegalArgumentException(DELETE_NOT_FOUND.message);
+        }
+        //TODO: fk check when Person is a fk
+        personRepository.deleteById(personId);
     }
 
     @Override
     @Transactional
     public void updatePerson(int personId, Person person) throws IllegalArgumentException, DataIntegrityViolationException {
+        if (RepositoryHelper.cannotFindId(personRepository, personId)) {
+            throw new IllegalArgumentException(UPDATE_NOT_FOUND.message);
+        }
+        if (hasForeignKeys(person) &&
+                RepositoryHelper.foreignKeyIsNotValid(personRepository, getListOfForeignKeyRepositories(), person)) {
+            throw new DataIntegrityViolationException(UPDATE_FOREIGN_KEY.message);
+        }
+        Person personToUpdate = RepositoryHelper.getById(personRepository, personId);
+        personToUpdate.setFirstName(person.getFirstName());
+        personToUpdate.setLastName(person.getLastName());
+        personToUpdate.setAge(person.getAge());
+        personToUpdate.setTitle(person.getTitle());
+        personToUpdate.setFk_race(person.getFk_race());
+        personToUpdate.setFk_wealth(person.getFk_wealth());
+        personToUpdate.setFk_ability_score(person.getFk_ability_score());
+        personToUpdate.setIsNPC(person.getIsNPC());
+        personToUpdate.setIsEnemy(person.getIsEnemy());
+        personToUpdate.setPersonality(person.getPersonality());
+        personToUpdate.setDescription(person.getDescription());
+        personToUpdate.setNotes(person.getNotes());
+    }
 
+    private boolean nameIsNullOrEmpty(String name) {
+        return isNull(name) || name.isEmpty();
+    }
+
+    private boolean personAlreadyExists(Person person) {
+        return personRepository.personExists(person).isPresent();
+    }
+
+    private boolean hasForeignKeys(Person person) {
+        return person.getFk_race() != null ||
+                person.getFk_wealth() != null ||
+                person.getFk_ability_score() != null;
+    }
+
+    private List<CrudRepository> getListOfForeignKeyRepositories() {
+        return new ArrayList<>(Arrays.asList(raceRepository, wealthRepository, abilityScoreRepository));
     }
 }
