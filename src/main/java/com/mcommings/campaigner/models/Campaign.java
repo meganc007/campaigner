aae0119892fe3f42b89a5f019519e034a2fdaf64
Megan Commings <meganc007@msn.com>
2023-09-15 15:25:22 -0500

    added Campaign and tests

---
+++ src/main/java/com/mcommings/campaigner/models/Campaign.java
@@ -0,0 +1,30 @@
+package com.mcommings.campaigner.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "campaigns")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@ToString(callSuper = true)
+public class Campaign extends BaseEntity {
+
+    @Id
+    @Column(name = "campaign_uuid", nullable = false, unique = true, columnDefinition = "varchar(36) default gen_random_uuid()")
+    private UUID uuid;
+
+    public Campaign () {
+        super();
+        this.uuid = UUID.randomUUID();
+    }
+
+    public Campaign(String name, String description) {
+        this();
+        this.setName(name);;
+        this.setDescription(description);
+    }
+}
---
+++ src/main/java/com/mcommings/campaigner/repositories/ICampaignRepository.java
@@ -0,0 +1,20 @@
+package com.mcommings.campaigner.repositories;
+
+import com.mcommings.campaigner.models.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ICampaignRepository extends JpaRepository<Campaign, Integer> {
+
+    Optional<Campaign> findByUuid(UUID uuid);
+
+    Optional<Campaign> findByName(String name);
+
+    void deleteByUuid(UUID uuid);
+
+    Boolean existsByUuid(UUID uuid);
+}
