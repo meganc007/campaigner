09c3e8ed9423b06701a340aac22d49f991503480
Megan Commings <meganc007@msn.com>
2024-03-03 11:48:53 -0600

    added ability to get all Places via fk_campaign_uuid, added tests

--- src/main/java/com/mcommings/campaigner/interfaces/locations/IPlace.java
+++ src/main/java/com/mcommings/campaigner/interfaces/locations/IPlace.java
@@ -3,12 +3,15 @@ package com.mcommings.campaigner.interfaces.locations;
 import com.mcommings.campaigner.models.locations.Place;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface IPlace {
 
     List<Place> getPlaces();
 
     Place getPlace(int placeId);
+
+    List<Place> getPlacesByCampaignUUID(UUID uuid);
 
     void savePlace(Place place);
 
