57df7a65f867644dcd451308ff824040365f5925
Megan Commings <meganc007@msn.com>
2024-03-03 16:35:46 -0600

    added bootstrap js

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import App from "./App";
 
