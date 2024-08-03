0de9a10c5af6b1ecb52a40294f7c5191113f438a
Megan Commings <meganc007@msn.com>
2024-08-03 15:26:41 -0500

    added City, cleanup

--- src/main/frontend/src/pages/CampaignOverview.jsx
+++ src/main/frontend/src/pages/CampaignOverview.jsx
@@ -1,24 +1,26 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Continent from "../components/Continent.jsx";
+import Country from "../components/Country.jsx";
+import City from "../components/City.jsx";
 
 export default function CampaignOverview(props) {
-  const location = useLocation();
-  const campaign = location.state.campaign;
+  const reactLocation = useLocation();
+  const campaign = reactLocation.state.campaign;
 
-  const [continents, setContinents] = useState([]);
+  const [location, setLocation] = useState({});
   useEffect(() => {
     fetch(
-      `http://localhost:8080/api/locations/continents/campaign/${campaign.uuid}`
+      `http://localhost:8080/api/locations/${campaign.uuid}`
     )
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         console.log(data);
-        setContinents(data);
+        setLocation(data);
       });
-  }, []);
+  }, [campaign.uuid]);
 
   return (
     <div className="container">
@@ -29,13 +31,13 @@ export default function CampaignOverview(props) {
           <p>{campaign.description}</p>
           <div className="container">
             <div className="row">
-              <div className="col-6">
+              <div className="col-12">
                 <div className="card">
                   <h2>Locations</h2>
-                  <Continent continents={continents} />
+                  <City cities={location.cities || []} />
                 </div>
               </div>
-              <div className="col-6">
+              <div className="col-12">
                 <div className="card">
                   <h2>People</h2>
                 </div>
