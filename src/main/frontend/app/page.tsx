9654380a0610090e50ad99951ec0827dddd4b23c
Megan Commings <meganc007@msn.com>
2024-12-01 17:42:47 -0600

    new frontend using nextjs, typescript, and tailwind

---
+++ src/main/frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <p>under construction</p>
+      <Link href="/campaign">Campaign Overview</Link>
+    </main>
+  );
+}
