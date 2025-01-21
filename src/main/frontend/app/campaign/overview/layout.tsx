cf71a2086774865626f89e514b4b55df51d2c89c
Megan Commings <meganc007@msn.com>
2025-01-20 18:41:44 -0600

    fixed layouts

--- src/main/frontend/app/campaign/overview/layout.tsx
+++ src/main/frontend/app/campaign/overview/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from "next";
-import "../globals.css";
+import "@/app/globals.css";
+import MenuTop from "@/app/components/nav/menu-top";
 
 export const metadata: Metadata = {
   title: "Campaigner",
   description: "Design your ttrpg campaign.",
 };
 
-export default function CampaignLayout({
+export default function OverviewLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <>
+      <header>
+        <MenuTop />
+      </header>
       <main>{children}</main>
     </>
   );
