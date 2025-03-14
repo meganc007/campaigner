8672ac19823beb4b2d658711904c430aa4b5ccb8
Megan Commings <meganc007@msn.com>
2025-03-13 21:55:51 -0500

    deleted unused classes, refactored SettlementType to use DTO

---
+++ src/main/java/com/mcommings/campaigner/locations/services/interfaces/ISettlementType.java
@@ -0,0 +1,18 @@
+package com.mcommings.campaigner.locations.services.interfaces;
+
+import com.mcommings.campaigner.locations.dtos.SettlementTypeDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ISettlementType {
+    List<SettlementTypeDTO> getSettlementTypes();
+
+    Optional<SettlementTypeDTO> getSettlementType(int settlementTypeId);
+
+    void saveSettlementType(SettlementTypeDTO settlementType);
+
+    void deleteSettlementType(int settlementTypeId);
+
+    Optional<SettlementTypeDTO> updateSettlementType(int settlementTypeId, SettlementTypeDTO settlementType);
+}
