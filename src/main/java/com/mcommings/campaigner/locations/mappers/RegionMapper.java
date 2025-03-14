375e7f23498ce763408b7aafe81377b70bf5a763
Megan Commings <meganc007@msn.com>
2025-03-13 22:14:23 -0500

    created DTOs and mappers for remaining location entities

---
+++ src/main/java/com/mcommings/campaigner/locations/mappers/RegionMapper.java
@@ -0,0 +1,13 @@
+package com.mcommings.campaigner.locations.mappers;
+
+import com.mcommings.campaigner.locations.dtos.RegionDTO;
+import com.mcommings.campaigner.locations.entities.Region;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface RegionMapper {
+
+    Region mapFromRegion(RegionDTO dto);
+
+    RegionDTO mapToRegionDto(Region region);
+}
