db5d21a067f60f90e49a7c68082683e1ca5f5f98
Megan Commings <meganc007@msn.com>
2025-03-15 19:50:32 -0500

    refactored entities

--- src/main/java/com/mcommings/campaigner/modules/items/entities/DiceType.java
+++ src/main/java/com/mcommings/campaigner/modules/items/entities/DiceType.java
@@ -1,32 +1,23 @@
 package com.mcommings.campaigner.modules.items.entities;
 
-import com.mcommings.campaigner.modules.common.entities.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@Builder
 @Entity
 @Table(name = "dice_types")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@ToString(callSuper = true)
-public class DiceType extends BaseEntity {
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiceType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-    @Column(name = "max_roll")
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String description;
     private int max_roll;
-
-    public DiceType() {
-        super();
-    }
-
-    public DiceType(int id, String name, String description, int max_roll) {
-        this.id = id;
-        this.setName(name);
-        this.setDescription(description);
-        this.max_roll = max_roll;
-    }
 }
--- src/main/java/com/mcommings/campaigner/modules/items/entities/Inventory.java
+++ src/main/java/com/mcommings/campaigner/modules/items/entities/Inventory.java
@@ -1,68 +1,29 @@
 package com.mcommings.campaigner.modules.items.entities;
 
-import com.mcommings.campaigner.modules.common.entities.Campaign;
-import com.mcommings.campaigner.modules.locations.entities.Place;
-import com.mcommings.campaigner.modules.people.entities.Person;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@Builder
 @Entity
 @Table(name = "inventory")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inventory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-    @Column(name = "fk_person", nullable = false)
-    private Integer fk_person;
-
-    @Column(name = "fk_item", nullable = false)
-    private Integer fk_item;
-
-    @Column(name = "fk_weapon")
-    private Integer fk_weapon;
-
-    @Column(name = "fk_place")
-    private Integer fk_place;
-
-    @Column(name = "fk_campaign_uuid")
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String description;
     private UUID fk_campaign_uuid;
-
-    @ManyToOne
-    @JoinColumn(name = "fk_campaign_uuid", referencedColumnName = "campaign_uuid", updatable = false, insertable = false)
-    private Campaign campaign;
-
-    @ManyToOne
-    @JoinColumn(name = "fk_person", referencedColumnName = "id", updatable = false, insertable = false)
-    private Person person;
-
-    @ManyToOne
-    @JoinColumn(name = "fk_item", referencedColumnName = "id", updatable = false, insertable = false)
-    private Item item;
-
-    @ManyToOne
-    @JoinColumn(name = "fk_weapon", referencedColumnName = "id", updatable = false, insertable = false)
-    private Weapon weapon;
-
-    @ManyToOne
-    @JoinColumn(name = "fk_place", referencedColumnName = "id", updatable = false, insertable = false)
-    private Place place;
-
-    public Inventory() {
-    }
-
-    public Inventory(int id, Integer fk_person, Integer fk_item, Integer fk_weapon, Integer fk_place, UUID fk_campaign_uuid) {
-        this.id = id;
-        this.fk_person = fk_person;
-        this.fk_item = fk_item;
-        this.fk_weapon = fk_weapon;
-        this.fk_place = fk_place;
-        this.fk_campaign_uuid = fk_campaign_uuid;
-    }
-
+    private Integer fk_person;
+    private Integer fk_item;
+    private Integer fk_weapon;
+    private Integer fk_place;
 }
