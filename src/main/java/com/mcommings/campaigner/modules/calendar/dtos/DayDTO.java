680505cc77e916620053e853a5d58b8bfa0656d7
Megan Commings <meganc007@msn.com>
2025-03-17 17:16:41 -0500

    added missing import

--- src/main/java/com/mcommings/campaigner/modules/calendar/dtos/DayDTO.java
+++ src/main/java/com/mcommings/campaigner/modules/calendar/dtos/DayDTO.java
@@ -1,6 +1,7 @@
 package com.mcommings.campaigner.modules.calendar.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
