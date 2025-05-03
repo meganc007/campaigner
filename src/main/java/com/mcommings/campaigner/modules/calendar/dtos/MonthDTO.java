4974dc131435c5169aca5a85e0759e7a215ca2f2
Megan Commings <meganc007@msn.com>
2025-05-03 16:41:02 -0500

    updated MonthController URI, added message to @NotNull annotation in MonthDTO, added and modified tests to MonthControllerTest

--- src/main/java/com/mcommings/campaigner/modules/calendar/dtos/MonthDTO.java
+++ src/main/java/com/mcommings/campaigner/modules/calendar/dtos/MonthDTO.java
@@ -19,7 +19,7 @@ public class MonthDTO {
     @NotBlank(message = "Month name cannot be empty")
     private String name;
     private String description;
-    @NotNull
+    @NotNull(message = "Campaign UUID cannot be null or empty.")
     private UUID fk_campaign_uuid;
     private String season;
 }
