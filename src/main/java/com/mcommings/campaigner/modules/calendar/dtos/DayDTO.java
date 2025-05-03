3f1abae5fc0f14a77672f257634df53a7123ff33
Megan Commings <meganc007@msn.com>
2025-05-03 16:33:34 -0500

    added message to @NotNull annotation in DayDTO, added and modified tests to DayControllerTest

--- src/main/java/com/mcommings/campaigner/modules/calendar/dtos/DayDTO.java
+++ src/main/java/com/mcommings/campaigner/modules/calendar/dtos/DayDTO.java
@@ -19,7 +19,7 @@ public class DayDTO {
     @NotBlank(message = "Day name cannot be empty")
     private String name;
     private String description;
-    @NotNull
+    @NotNull(message = "Campaign UUID cannot be null or empty.")
     private UUID fk_campaign_uuid;
     private Integer fk_week;
 }
