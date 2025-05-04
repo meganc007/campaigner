050cc4a8eff8f8a320f8f60d108cc828c2d000df
Megan Commings <meganc007@msn.com>
2025-05-03 20:20:25 -0500

    added method to controller and service, added message to @NotNull annotation in CountryDTO, added CountryControllerTest

--- src/main/java/com/mcommings/campaigner/modules/locations/dtos/CountryDTO.java
+++ src/main/java/com/mcommings/campaigner/modules/locations/dtos/CountryDTO.java
@@ -19,7 +19,7 @@ public class CountryDTO {
     @NotBlank(message = "Country name cannot be empty")
     private String name;
     private String description;
-    @NotNull
+    @NotNull(message = "Campaign UUID cannot be null or empty.")
     private UUID fk_campaign_uuid;
     private Integer fk_continent;
     private Integer fk_government;
