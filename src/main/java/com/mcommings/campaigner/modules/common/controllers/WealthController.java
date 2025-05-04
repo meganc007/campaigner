89a6a77ad249ce934bb332ed0aa3ed13e6815e06
Megan Commings <meganc007@msn.com>
2025-05-04 13:28:41 -0500

    actually added WealthControllerTest this time, fixed URI

--- src/main/java/com/mcommings/campaigner/modules/common/controllers/WealthController.java
+++ src/main/java/com/mcommings/campaigner/modules/common/controllers/WealthController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "api/common/wealth")
+@RequestMapping(path = "api/wealth")
 public class WealthController {
     private final IWealth wealthService;
 
