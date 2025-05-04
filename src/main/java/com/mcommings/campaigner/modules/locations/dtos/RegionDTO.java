185970296a1d61be4229b712a552cec9176a695d
Megan Commings <meganc007@msn.com>
2025-05-03 20:50:48 -0500

    added message to @NotNull annotation in RegionDTO, added RegionControllerTest

--- src/main/java/com/mcommings/campaigner/modules/locations/dtos/RegionDTO.java
+++ src/main/java/com/mcommings/campaigner/modules/locations/dtos/RegionDTO.java
@@ -19,7 +19,7 @@ public class RegionDTO {
     @NotBlank(message = "Region name cannot be empty")
     private String name;
     private String description;
-    @NotNull
+    @NotNull(message = "Campaign UUID cannot be null or empty.")
     private UUID fk_campaign_uuid;
     private Integer fk_country;
     private Integer fk_climate;
