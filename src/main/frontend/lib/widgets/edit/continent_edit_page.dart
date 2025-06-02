c52554aad3c7b295e82e9d5991b6997ce8424878
Megan Commings <meganc007@msn.com>
2025-06-02 17:00:04 -0500

    organization, placeholder page, created reusable widgets

---
+++ src/main/frontend/lib/widgets/edit/continent_edit_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ContinentEditPage extends StatelessWidget {
+  final int continentId;
+  const ContinentEditPage({super.key, required this.continentId});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("under construction");
+  }
+}
---
+++ src/main/frontend/lib/widgets/styled_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class StyledTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final int maxLines;
+  final String? Function(String?)? validator;
+  const StyledTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.maxLines = 1,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFDF6E3),
+        border: Border.all(width: 2, color: Colors.brown.shade700),
+      ),
+      child: TextFormField(
+        controller: controller,
+        maxLines: maxLines,
+        validator: validator,
+        decoration: InputDecoration(
+          labelText: label,
+          border: InputBorder.none,
+          isDense: true,
+        ),
+        style: const TextStyle(fontFamily: "monospace", fontSize: 14),
+      ),
+    );
+  }
+}
