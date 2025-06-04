43cf02d94dbc973c9675948593b7daa3e3f0977b
Megan Commings <meganc007@msn.com>
2025-06-04 17:51:58 -0500

    made CityDetailPage stateful and added needed variables

--- src/main/frontend/lib/widgets/details/locations/city_detail_page.dart
+++ src/main/frontend/lib/widgets/details/locations/city_detail_page.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CityDetailPage extends StatelessWidget {
+class CityDetailPage extends StatefulWidget {
   final String uuid;
-  const CityDetailPage({super.key, required this.uuid});
+  final Map<int, String> wealthMap;
+  final Map<int, String> countryMap;
+  final Map<int, String> settlementTypeMap;
+  final Map<int, String> governmentMap;
+  final Map<int, String> regionMap;
+  const CityDetailPage({
+    super.key,
+    required this.uuid,
+    required this.wealthMap,
+    required this.countryMap,
+    required this.settlementTypeMap,
+    required this.governmentMap,
+    required this.regionMap,
+  });
 
+  @override
+  State<CityDetailPage> createState() => _CityDetailPageState();
+}
+
+class _CityDetailPageState extends State<CityDetailPage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
