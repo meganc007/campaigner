5db6c89a8f8050c886eac566607948b111e3b791
Megan Commings <meganc007@msn.com>
2026-03-11 20:16:13 -0600

    City - created view, create, and update dtos, updated mapper, updated entity, removed deprecated dto, removed deprecated interface, updated repository

--- src/main/java/com/mcommings/campaigner/modules/locations/mappers/CityMapper.java
+++ src/main/java/com/mcommings/campaigner/modules/locations/mappers/CityMapper.java
@@ -1,12 +1,31 @@
 package com.mcommings.campaigner.modules.locations.mappers;
 
-import com.mcommings.campaigner.modules.locations.dtos.CityDTO;
+import com.mcommings.campaigner.config.GlobalMapperConfig;
+import com.mcommings.campaigner.modules.locations.dtos.cities.CreateCityDTO;
+import com.mcommings.campaigner.modules.locations.dtos.cities.UpdateCityDTO;
+import com.mcommings.campaigner.modules.locations.dtos.cities.ViewCityDTO;
 import com.mcommings.campaigner.modules.locations.entities.City;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper
+@Mapper(config = GlobalMapperConfig.class)
 public interface CityMapper {
-    City mapFromCityDto(CityDTO dto);
 
-    CityDTO mapToCityDto(City city);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "campaign", ignore = true)
+    City toEntity(CreateCityDTO dto);
+
+    @Mapping(source = "campaign.uuid", target = "campaignUuid")
+    @Mapping(source = "wealth.id", target = "wealthId")
+    @Mapping(source = "country.id", target = "countryId")
+    @Mapping(source = "settlementType.id", target = "settlementTypeId")
+    @Mapping(source = "government.id", target = "governmentId")
+    @Mapping(source = "region.id", target = "regionId")
+    ViewCityDTO toDto(City city);
+
+    void updateCityFromDto(
+            UpdateCityDTO dto,
+            @MappingTarget City entity
+    );
 }
